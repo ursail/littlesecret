f4ee972fa9d42732a5ebaeb9b264bf2684390574
Ursail <urs.mi8@gmail.com>
2026-06-20 17:58:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function toggleLanguage(){
-  const html=document.documentElement;
-  const next=html.lang==='de'?'en':'de';
-  html.lang=next;
-  document.querySelectorAll('[data-de][data-en]').forEach(el=>{
-    el.textContent=el.dataset[next];
-  });
-}
